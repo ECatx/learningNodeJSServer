0de7c82ff00d280c8edfd92037142ce10c5d8c55
Eric Castellanos <eric@ericcastellanos.com>
2021-04-03 22:07:07 -0500

    added notes endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import root from './root'
+import api from './api'
 
 const router = Router()
 
 router.use(root)
+router.use('/eric', api)
 
 export default router
