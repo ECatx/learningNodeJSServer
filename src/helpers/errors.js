13ac3e63f983bceba0d915568d80841e74de010b
Eric Castellanos <eric@ericcastellanos.com>
2021-04-03 20:04:42 -0500

    configurations

---
+++ src/helpers/errors.js
@@ -0,0 +1,21 @@
+import logger from './logger'
+
+export const notFound = (req, res, next) => {
+  const error = new Error(`Not Fount - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+}
+
+// eslint-disable-next-line no-unused-vars
+export const errorController = (error, req, res, next) => {
+  const statusCode = res.status === 200 ? 500 : res.statusCode
+  logger.error({
+    message: error.message,
+    stack: error.stack,
+  })
+  res.status(statusCode)
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🐈' : error.stack,
+  })
+}
